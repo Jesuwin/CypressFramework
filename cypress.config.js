435d705e95fa497943a5e044ea985071e3806141
Jesuwin <63298598+Jesuwin@users.noreply.github.com>
2023-02-27 08:07:47 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "4myuz9",
+    projectId: "4myuz9",
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
+    
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
